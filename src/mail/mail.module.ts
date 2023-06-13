2c389c9c1a3b3097e1b1f353d8dca06ae0e5aaff
andregodasi <andre.godasi@gmail.com>
2023-06-12 23:19:26 -0300

    chore: config email

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -10,6 +10,7 @@ import { MailService } from './mail.service';
       transport: {
         host: process.env.EMAIL_HOST,
         port: Number(process.env.EMAIL_PORT),
+        secure: false,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASSWORD,
