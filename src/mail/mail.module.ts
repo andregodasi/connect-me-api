9b9752d7ab28b7bfbbc299ff482c53e23613b11d
andregodasi <andre.godasi@gmail.com>
2023-06-18 17:16:10 -0300

    chore: config proveder email

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -4,29 +4,53 @@ import { join } from 'path';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { MailService } from './mail.service';
 
+const maiilModuleEnvironment =
+  process.env.ENVIRONMENT === 'local'
+    ? MailerModule.forRoot({
+        transport: {
+          host: process.env.EMAIL_HOST,
+          port: Number(process.env.EMAIL_PORT),
+          secure: false,
+          auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD,
+          },
+        },
+        defaults: {
+          from: 'connect.me.develop@gmail.com',
+        },
+        preview: true,
+        template: {
+          dir: join(__dirname, '..', 'utils', 'emails', 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      })
+    : MailerModule.forRoot({
+        transport: {
+          service: 'gmail',
+          auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD,
+          },
+        },
+        defaults: {
+          from: 'connect.me.develop@gmail.com',
+        },
+        preview: true,
+        template: {
+          dir: join(__dirname, '..', 'utils', 'emails', 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      });
+
 @Module({
-  imports: [
-    MailerModule.forRoot({
-      transport: {
-        service:'gmail',
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASSWORD,
-        },
-      },
-      defaults: {
-        from: 'connect.me.develop@gmail.com',
-      },
-      preview: true,
-      template: {
-        dir: join(__dirname, '..', 'utils', 'emails', 'templates'),
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
-        },
-      },
-    }),
-  ],
+  imports: [maiilModuleEnvironment],
   providers: [MailService],
   exports: [MailService],
 })
